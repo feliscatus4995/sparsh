02ad39d7289ea8ad59522b718d96f5d16b8c8504
Prarthana Pande <60259152+feliscatus4995@users.noreply.github.com>
2021-04-11 18:49:01 +0530

    Deleting unwanted directories

--- customer/Valkyrie/web/src/lib/api/getSocket.ts
+++
@@ -1,6 +0,0 @@
-import socketIOClient from 'socket.io-client';
-
-export const getSocket = () => socketIOClient(`${process.env.REACT_APP_API!}/ws`, {
-  transports: ['websocket'],
-  upgrade: false
-});
